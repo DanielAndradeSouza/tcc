b9554875fecfc369a7b214769a8b3e99351a3b87
Daniel <dansouza359157@gmail.com>
2025-04-03 16:27:19 -0300

    instalando alguns pacotes do next.js e do express

--- back-end/tcc-back-end/src/main.ts
+++ back-end/tcc-back-end/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as express from 'express';
 async function bootstrap() {
+  const express = require('express');
+  const server = express();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
