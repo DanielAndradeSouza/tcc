cc1e6940c4244e576c980b790dea0c5970f39e6f
Daniel <dansouza359157@gmail.com>
2025-04-03 15:30:35 -0300

    Table dto

--- back-end/tcc-back-end/src/Entidades/table/dto/create-table.dto.ts
+++ back-end/tcc-back-end/src/Entidades/table/dto/create-table.dto.ts
@@ -1,1 +1,15 @@
-export class CreateTableDto {}
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateTableDto {
+    @IsNotEmpty()
+    @IsString()
+    tableName: string;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @IsBoolean()
+    active?: boolean;
+}
