4e6c8a2a97d358b4a08f41a9d6c1cfa5f689f8dc
Daniel <dansouza359157@gmail.com>
2025-04-03 15:18:15 -0300

    dto do usuário

--- back-end/tcc-back-end/src/Entidades/user/dto/create-user.dto.ts
+++ back-end/tcc-back-end/src/Entidades/user/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import { IsBoolean, IsDate, IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class CreateUserDto {
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+
+    @Transform(({ value }) => new Date(value))
+    @IsDate()
+    creation_date: Date;
+
+    @IsBoolean()
+    active: boolean;
+
+    @IsOptional()
+    @Transform(({ value }) => (value ? new Date(value) : null))
+    @IsDate()
+    deactivation_date?: Date;
+}
