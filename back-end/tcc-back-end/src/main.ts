f59ea9d324d92e24b3ec6ba3f9bcb5d7a66a70f6
Daniel <dansouza359157@gmail.com>
2025-04-03 16:38:53 -0300

    Estudando e também criando alguns diretórios para o projeto do next.

--- back-end/tcc-back-end/src/main.ts
+++ back-end/tcc-back-end/src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
 async function bootstrap() {
-  const express = require('express');
-  const server = express();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
