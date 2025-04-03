15d87af5caaa72aae497ff465027a9c2cca60430
Daniel <dansouza359157@gmail.com>
2025-04-03 15:31:17 -0300

    Scene_image dto

--- back-end/tcc-back-end/src/Entidades/scene_images/dto/create-scene_image.dto.ts
+++ back-end/tcc-back-end/src/Entidades/scene_images/dto/create-scene_image.dto.ts
@@ -1,1 +1,35 @@
-export class CreateSceneImageDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateSceneImageDto {
+    @IsNotEmpty()
+    @IsInt()
+    sceneId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    height: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    width: number;
+
+    @IsNotEmpty()
+    @IsString()
+    imageUrl: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    xPos: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    yPos: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    rotation: number;
+
+    @IsNotEmpty()
+    @IsString()
+    layer: string;
+}
