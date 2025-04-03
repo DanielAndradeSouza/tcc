dbadf37925ea66cc2cb35c5446edabafdb352fe4
Daniel <dansouza359157@gmail.com>
2025-04-03 15:34:31 -0300

    mais um dto

--- back-end/tcc-back-end/src/Entidades/user_table/dto/create-user_table.dto.ts
+++ back-end/tcc-back-end/src/Entidades/user_table/dto/create-user_table.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserTableDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateUserTableDto {
+    @IsNotEmpty()
+    @IsInt()
+    userId: number;
+
+    @IsNotEmpty()
+    @IsInt()
+    tableId: number;
+
+    @IsNotEmpty()
+    @IsString()
+    role: string;
+
+    @IsOptional()
+    joinDate?: Date;
+}
