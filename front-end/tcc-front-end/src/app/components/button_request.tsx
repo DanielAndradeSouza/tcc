368f6240af227a4d584f4e6de3694a8504e95cf1
Daniel <dansouza359157@gmail.com>
2025-04-09 19:50:27 -0300

    criando um componente de botão para ser utilizado de forma genérica

---
+++ front-end/tcc-front-end/src/app/components/button_request.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { fetchData } from "../services/api";
+
+function button_request(show_text:string,type_req:string){
+  const [usuarios, setUsuarios] = useState<any[]>([]);
+    const handleClick = async () => {
+        try {
+          const data = await fetchData(type_req);
+          setUsuarios(data); 
+        } catch (err: any) {
+          console.error(err);
+        }
+      };
+    return (<div>
+        <button
+        onClick={handleClick}
+        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer">
+          {show_text}
+        </button>
+
+        <ul className="mt-4">
+        {usuarios.map((usuario, index) => (
+          <li key={index}>{JSON.stringify(usuario)}</li>
+        ))}
+      </ul>
+    </div>);
+}
